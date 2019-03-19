a8e673fb5763230e3e58f5fcb0f42717164d0119
leehwijin <cysoon96@gmail.com>
2019-03-19 19:39:05 +0900

    Make internsheep-back

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path');
+require('./db/mongo') //connect db
+const aimsLogin = require('./routes/aimsLogin')
+const app = express()
+app.use('/aimsLogin', aimsLogin)
+
+
+module.exports = app
---
+++ constants.js
@@ -0,0 +1,5 @@
+module.exports = {
+    SECRET:"WeBsYsTeMtEaMpRoJeCt12",
+    PORT: 8888,
+    DB: "mongodb://localhost:27017/last"
+}
