adc01982238c826703ff4cf46665c0234ba35a17
suyeon <jse9789@ajou.ac.kr>
2019-04-11 15:58:04 +0900

    add webpack

--- app.js
+++ app.js
@@ -3,14 +3,10 @@ const bodyParser = require('body-parser')
 const path = require('path');
 require('./db/mongo') //connect db
 
-const resume = require('./routes/resume')
-
 
 const aimsLogin = require('./routes/aimsLogin')
 const app = express()
 app.use('/aimsLogin', aimsLogin)
-
-app.use('/resume', resume)
 
 
 module.exports = app
