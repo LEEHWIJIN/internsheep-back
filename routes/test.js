66bdc9c0a680f72614af7b9663a32907933cfca5
suyeon <jse9789@ajou.ac.kr>
2019-04-15 19:12:46 +0900

    Divide router and db folder

--- routes/test.js
+++ routes/test.js
@@ -1,18 +1,14 @@
 const Router = require('express')
 const router = Router()
-//const connection = require('../db/mysql')
-var mysql = require('mysql');
-var conf = require('../constants')
-var connection = mysql.createConnection(conf)
+var mysql = require('../db/mysql')
 
 router.get('/', function(req,res){
     res.send('Root');
 })
 
 router.get('/persons', function(req, res){
-
-    connection.connect()
-    connection.query('SELECT * from Persons', function(err, rows){
+    var conn = mysql.connection
+    conn.query('SELECT * from Persons', function(err, rows){
         if(err) console.log(err)
 
         console.log('The solution is : ', rows)
