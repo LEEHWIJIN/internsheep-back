0617cdd4a5c12c6cc8a6b0240d27516f9c0583a3
hol819 <hol819@naver.com>
2019-04-29 20:33:40 +0900

    change db name to CompanyNotice

--- routes/coMypage.js
+++ routes/coMypage.js
@@ -6,6 +6,22 @@ var conn = mysql()
 router.post('/writeNotice', function(req, res){
     var cName = req.body.cName
     var cManagerName = req.body.cManagerName
+    var sql = 'INSERT INTO CompanyNotice (cName, cManagerName) VALUES(?,?)'
+    var params = [cName, cManagerName]
+
+    conn.init().query(sql, params, function(err, rows)
+    {
+        if(err) console.log(err)
+        else
+        {
+            console.log(rows)
+            res.send(rows)
+        }
+    })
+})
+router.post('/writeNotice', function(req, res){
+    var cName = req.body.cName
+    var cManagerName = req.body.cManagerName
     var sql = 'INSERT INTO CompanyAnnouncement (cName, cManagerName) VALUES(?,?)'
     var params = [cName, cManagerName]
 
