d1dfb36e05c5d29e0629f6732d6e58ee0f725517
hol819 <hol819@naver.com>
2019-04-29 19:55:52 +0900

    post cName, cManagerName to cAnnouncement db

---
+++ routes/coMypage.js
@@ -0,0 +1,23 @@
+const Router = require('express')
+const router = Router()
+const mysql = require('../db/database_config.js')
+var conn = mysql()
+
+router.post('/writeNotice', function(req, res){
+    var cName = req.body.cName
+    var cManagerName = req.body.cManagerName
+    var sql = 'INSERT INTO CompanyAnnouncement (cName, cManagerName) VALUES(?,?)'
+    var params = [cName, cManagerName]
+
+    conn.init().query(sql, params, function(err, rows)
+    {
+        if(err) console.log(err)
+        else
+        {
+            console.log(rows)
+            res.send(rows)
+        }
+    })
+})
+
+module.exports = router
