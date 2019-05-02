a6e3191f6a14d01615ca3b4a8b258e9b0948eff3
suyeon <jse9789@ajou.ac.kr>
2019-05-02 16:35:56 +0900

    Modify if not apply, return false

--- routes/mypage.js
+++ routes/mypage.js
@@ -1,7 +1,18 @@
 const Router = require('express')
 const router = Router()
+const multer = require("multer")
 const mysql = require('../db/database_config.js')
 var conn = mysql()
+
+let storage = multer.diskStorage({
+    destination: function (req,file,cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req,file,cb) {
+        cb(null, file.originalname)
+    }
+})
+var upload = multer({storage:storage})
 
 router.post('/resume', function(req, res){
     var sNum = req.body.sNum
@@ -74,8 +85,33 @@ router.get('/applyStatus', function(req, res){
                     responseData[0] = rows[i]
                 }
             }
-            console.log(responseData)
-            return res.json(responseData)
+            if(responseData==null){
+                return res.send(false)
+            }
+            else {
+                console.log(responseData)
+                return res.json(responseData)
+            }
+        }
+    })
+})
+
+router.post('/postReportAndReview', upload.single('user_report'), function(req, res){
+    let file = req.file
+    let result = {
+        originalname: file.originalname,
+        size:file.size
+    }
+    var sql = 'INSERT INTO report (sName, reportContent) VALUES(?,?)'
+    var sName = req.body.sName
+    var reportContent = result
+    var params = [sName,reportContent]
+
+    conn.init().query(sql,params, function(err, rows){
+        if(err) console.log(err)
+        else {
+            console.log(rows)
+            res.send(rows)
         }
     })
 })
