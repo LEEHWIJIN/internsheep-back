b0bc06c65cc8de509728497f90236a2b2b46d058
hol819 <hol819@naver.com>
2019-05-02 21:04:22 +0900

    delet watchApplyStd

--- routes/coMypage.js
+++ routes/coMypage.js
@@ -4,17 +4,9 @@ const mysql = require('../db/database_config.js')
 var conn = mysql()
 
 
-router.get('/watchApplyStd', function(req, res){
-    var sql = 'SELECT*FROM studentApplyCompany WHERE cName = ?'
-    var cName = req.body.cName
-    conn.init().query(sql, cName, function(err, rows){
-        if(err) console.log(err)
-        else {
-            console.log(rows)
-            res.send(rows)
-        }
-    })
-})
+
+
+
 
 router.post('/postApplyStd', function(req, res){
     var sql = 'UPDATE studentApplyCompany SET YN = ? WHERE sName = ?'
