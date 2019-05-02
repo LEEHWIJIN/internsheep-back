a4194a0b955937d0e41501d1d81abf12540c804d
hol819 <hol819@naver.com>
2019-05-02 15:44:54 +0900

    add watch, post ApplyStd

--- routes/coMypage.js
+++ routes/coMypage.js
@@ -3,12 +3,27 @@ const router = Router()
 const mysql = require('../db/database_config.js')
 var conn = mysql()
 
-var sql = 'INSERT INTO Resume VALUES(?,?)'
-router.get('/resume', function(req, res){
-    conn.init().query(sql,[res.sName, res.sNum], function(err, rows){
+
+router.get('/watchApplyStd', function(req, res){
+    var sql = 'SELECT*FROM studentApplyCompany WHERE cName = ?'
+    var cName = req.body.cName
+    conn.init().query(sql, cName, function(err, rows){
         if(err) console.log(err)
         else {
-            console.log(res.insertId)
+            console.log(rows)
+            res.send(rows)
+        }
+    })
+})
+
+router.post('/postApplyStd', function(req, res){
+    var sql = 'UPDATE studentApplyCompany SET YN = ? WHERE sName = ?'
+    var sName = req.body.sName
+    var YN = req.body.YN
+    conn.init().query(sql, YN, sName, function(err, rows){
+        if(err) console.log(err)
+        else {
+            console.log(rows)
             res.send(rows)
         }
     })
