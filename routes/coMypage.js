e0c2364fed455a6d46938af28c2cb4247f11beda
hol819 <hol819@naver.com>
2019-05-05 23:39:30 +0900

    add watchApplyStudent

--- routes/coMypage.js
+++ routes/coMypage.js
@@ -4,7 +4,66 @@ const mysql = require('../db/database_config.js')
 var conn = mysql()
 
 
+router.get('/watchApplyStd', function(req, res) {
 
+    Promise.resolve()
+        .then(firstSql)
+        .then(secondSql)
+        .catch(function (err) {
+            console.log('Error', err)
+            process.exit()
+        })
+
+    function firstSql() {
+        var sql = 'SELECT* FROM studentApplyCompany WHERE cName = ?'
+        var cName = req.body.cName
+        var sNames = []
+        return new Promise(function (resolve, reject) {
+            conn.init().query(sql, cName, function (err, rows) {
+                if (err) reject(err)
+                else {
+                    for (var i = 0; i < rows.length; i++) {
+                        sNames[i] = rows[i].sName
+                    }
+                    resolve(sNames)
+                }
+            })
+        })
+    }
+
+    function secondSql(sNames) {
+        var sql='SELECT* FROM resume WHERE sName = ?'
+        var count = 0
+        console.log(sNames.length)
+
+        for (var i = 
+            0; i < sNames.length; i++) {
+            if (count == 0) {
+                count++
+                continue
+            }
+            else {
+                sql += ' union '
+                sql += 'SELECT* FROM resume WHERE sName = ?'
+            }
+        }
+
+        return new Promise(function (resolve, reject) {
+            console.log(sql)
+            console.log(sNames)
+            conn.init().query(sql, sNames, function (err, rows) {
+                if(sNames.length == 0)
+                    res.send('no result')
+                else if (err) reject(err)
+                else {
+                    res.json(rows)
+                    console.log(rows)
+                    resolve(rows)
+                }
+            })
+        })
+    }
+})
 
 
 
