77cce65a8c8f588aeaa947e6396ad5ec01b3a364
hol819 <hol819@naver.com>
2019-05-15 16:34:32 +0900

    delete reduplication code

--- routes/coMypage.js
+++ routes/coMypage.js
@@ -264,19 +264,7 @@ router.get('/showApplyNotice', function(req, res){
 
 
 
-router.post('/postApplyStd', function(req, res){
-    var sql = 'UPDATE stdApplyCo SET YN = ? WHERE sName = ?'
-    var sName = req.body.sName
-    var YN = req.body.YN
-    var params = [YN, sName]
-    conn.init().query(sql, params, function(err, rows){
-        if(err) console.log(err)
-        else {
-            console.log(rows)
-            res.send(rows)
-        }
-    })
-})
+
 
 router.get('/showStdAttendence', function(req, res) {
 
@@ -349,7 +337,7 @@ router.get('/showStdAttendence', function(req, res) {
             })
         })
     }
-
+})
 router.get('/watchApplyStd', function(req, res) {
 
     Promise.resolve()
@@ -447,100 +435,6 @@ router.get('/watchApplyStd', function(req, res) {
 
 
 
-router.get('/watchApplyStd', function(req, res) {
-
-    Promise.resolve()
-        .then(firstSql)
-        .then(secondSql)
-        .then(getApplyStd)
-        .then(showResumeYN)
-        .catch(function (err) {
-            console.log('Error', err)
-            process.exit()
-        })
-
-    function firstSql() {
-        var sql = 'SELECT * FROM company, companyNotice WHERE company.cID = companyNotice.cID AND cName = ?'
-        var cName = req.query.cName
-        return new Promise(function (resolve, reject) {
-            conn.init().query(sql, cName, function (err, rows) {
-                if (err) reject(err)
-                else {
-                    if(rows.length == 0)
-                        res.send('공고가 없음')
-                    else
-                        resolve(rows[0].cNoticeID)
-                }
-            })
-        })
-    }
-
-    function secondSql(cNoticeID) {
-        var sql='SELECT* FROM applyNotice WHERE cNoticeId = ? '
-
-        return new Promise(function (resolve, reject) {
-            conn.init().query(sql, cNoticeID, function (err, rows) {
-                if (err) reject(err)
-                else {
-                    if(rows.length == 0)
-                        res.send('공고 신청을 하지 않았음')
-                    else
-                    {
-                        console.log(rows)
-                        resolve(rows[0].applyNoticeID)    
-                    }
-                }
-            })
-        })
-    }
-
-    function getApplyStd(applyNoticeID)
-    {
-        var sql = 'SELECT * FROM stdApplyCo WHERE applyNoticeID = ?'
-        return new Promise(function (resolve, reject) {
-            conn.init().query(sql, applyNoticeID, function (err, rows) {
-                if (err) reject(err)
-                else {
-                    if(rows.length == 0)
-                        res.send('신청한 학생이 없음')
-                    else
-                    {
-                        console.log(rows) 
-                        resolve(rows)    
-                    }
-                }
-            })
-        })
-    }
-    
-    function showResumeYN(resolvedRows)
-    {
-        console.log(resolvedRows)
-        var sql = 'SELECT * FROM resume, stdApplyCo WHERE resume.sID = stdApplyCo.sID AND stdApplyCoID = ?'
-        var sIDs = new Array()
-        sIDs[0] = resolvedRows[0].stdApplyCoID
-        var index = 1
-        console.log(sIDs)
-        for (var i = 0; i < resolvedRows.length-1; i++) {
-            sql += ' UNION '
-            sql += 'SELECT * FROM resume, stdApplyCo WHERE resume.sID = stdApplyCo.sID AND stdApplyCoID = ?'
-            sIDs[index] = resolvedRows[index].stdApplyCoID
-            console.log(sIDs[index])
-            index++;
-        }
-
-        return new Promise(function (resolve, reject) {
-            conn.init().query(sql, sIDs, function (err, rows) {
-                if (err) reject(err)
-                else 
-                {
-                    console.log(rows)
-                    res.json(rows)
-                }
-            })
-        })
-    }
-})
 
 
 
@@ -560,6 +454,6 @@ router.post('/changeYNApplyStd', function(req, res){
 
 
 
-})
+
 
 module.exports = router
