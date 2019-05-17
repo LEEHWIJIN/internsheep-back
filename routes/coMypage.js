6aa2bdb3a06a94f692579b77a1f7b9567a205901
hol819 <hol819@naver.com>
2019-05-18 00:47:14 +0900

    add watchApplyStdNum

--- routes/coMypage.js
+++ routes/coMypage.js
@@ -648,6 +648,145 @@ router.get('/watchApplyStd', function(req, res) {
         })
     }
 })
+router.get('/watchApplyStdNum', function(req, res) {
+
+    Promise.resolve()
+        .then(getApplyTermID)
+        .then(getcNoticeID)
+        .then(getApplyNoticeID)
+        .then(getApplyStd)
+        .then(showResumeYN)
+        .catch(function (err) {
+            console.log('Error', err)
+            process.exit()
+        })
+
+    function getApplyTermID() {
+        var sql = 'SELECT * FROM applyTerm WHERE applySemester = ? AND applyOrder = ?'
+        var semester = req.query.applySemester
+        var order = req.query.applyOrder
+        var sqlParams = [semester,order]
+        return new Promise(function (resolve, reject) {
+            conn.init().query(sql, sqlParams, function (err, rows) {
+                if (err) reject(err)
+                else {
+                    console.log(rows)
+                    if (rows.length==0)
+                    {
+                        res.send('기간이 없음')
+                    }
+                    else
+                    {
+                        resolve(rows[0].applyTermID)                       
+                    }
+                }
+            })
+        })
+    }
+    function getcNoticeID(applyTermID)
+    {
+        if (!applyTermID)
+        {
+            var params = [0,0]
+            resolve(params)
+        }
+        var sql = "SELECT * FROM companyNotice, company WHERE company.cID = companyNotice.cID AND cLoginID = ?"
+        var cLoginID = req.query.cLoginID
+        return new Promise(function (resolve, reject) {
+            conn.init().query(sql, cLoginID, function (err, rows) {
+                if (err) reject(err)
+                else {
+                    if (rows.length==0)
+                    {
+                        res.send('공고가 없음')
+                        var params = [0,0]
+                        resolve(params)
+                    }
+                    else
+                    {
+                        console.log(rows)
+                        var params =[rows[0].cNoticeID, applyTermID]
+                        console.log(params)
+                        resolve(params)
+                    }
+                }
+            })
+        })
+    }
+
+    function getApplyNoticeID(params) {
+        console.log(params)
+        var sql = 'SELECT * FROM applyNotice WHERE cNoticeID = ? AND applyTermID = ?'
+        return new Promise(function (resolve, reject) {
+            if (params[0]==0 && params[1]==0)
+                resolve(0)
+            else
+            {
+                conn.init().query(sql, params, function (err, rows)
+                {
+                    if (err) reject(err)
+                    else {
+                        if(rows.length == 0)
+                            res.send('공고 신청을 하지 않음')
+                        else
+                        {
+                            console.log(rows)
+                            resolve(rows[0].applyNoticeID)
+                        }
+                    }
+                })
+            }
+        })
+    }
+
+    
+    function getApplyStd(applyNoticeID)
+    {
+        var sql = 'SELECT * FROM stdApplyCo WHERE applyNoticeID = ?'
+        return new Promise(function (resolve, reject) {
+            conn.init().query(sql, applyNoticeID, function (err, rows) {
+                if (err) reject(err)
+                else {
+                    if(rows.length == 0)
+                        res.send('신청한 학생이 없음')
+                    else
+                    {
+                        console.log(rows) 
+                        resolve(rows)    
+                    }
+                }
+            })
+        })
+    }
+    
+    function showResumeYN(resolvedRows)
+    {
+        console.log(resolvedRows)
+        var sql = 'SELECT * FROM resume, student, stdApplyCo WHERE resume.sID = stdApplyCo.sID AND student.sID = stdApplyCo.sID AND stdApplyCoID = ?'
+        var sIDs = new Array()
+        sIDs[0] = resolvedRows[0].stdApplyCoID
+        var index = 1
+        console.log(sIDs)
+        for (var i = 0; i < resolvedRows.length-1; i++) {
+            sql += ' UNION '
+            sql += 'SELECT * FROM resume, student, stdApplyCo WHERE resume.sID = stdApplyCo.sID AND student.sID = stdApplyCo.sID AND stdApplyCoID = ?'
+            sIDs[index] = resolvedRows[index].stdApplyCoID
+            console.log(sIDs[index])
+            index++;
+        }
+
+        return new Promise(function (resolve, reject) {
+            conn.init().query(sql, sIDs, function (err, rows) {
+                if (err) reject(err)
+                else 
+                {
+                    console.log(rows.length)
+                    res.json(`"`+rows.length+`"`)
+                }
+            })
+        })
+    }
+})
 
 
 
