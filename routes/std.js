eee645f893cd7806da3b5926769c0504884f93e2
suyeon <jse9789@ajou.ac.kr>
2019-05-17 22:43:52 +0900

    Modify all list

--- routes/std.js
+++ routes/std.js
@@ -5,16 +5,16 @@ var conn = mysql()
 
 
 router.get('/list', function(req, res){
-    var sql = 'SELECT an.applyStdNum, cn.cBenefit, cn.cPay, cn.internTermStart, cn.internTermEnd, cn.cOccupation, cn.cNumOfPeople, cn.cTag, co.cName, co.cManagerName, co.cManagerPhone, co.cImage, co.cLocation FROM applyTerm at NATURAL JOIN applyNotice an NATURAL JOIN companyNotice cn NATURAL JOIN company co WHERE at.applySemester =? and at.applyOrder =?'
-    var params = [req.body.applySemester, req.body.applyOrder]
+    var sql = 'SELECT * FROM applyTerm at NATURAL JOIN applyNotice an NATURAL JOIN companyNotice cn NATURAL JOIN company co WHERE at.applySemester =? and at.applyOrder =?'
+    var params = [req.query.applySemester, req.query.applyOrder]
     conn.init().query(sql,params,function(err, rows){
         var responseData= []
         if(err) console.log(err)
         else {
-            console.log(rows)
             for (var i = 0; i < rows.length; i++) {
                 responseData[i] = rows[i]
             }
+            //console.log(responseData)
             return res.json(responseData)
         }
     })
